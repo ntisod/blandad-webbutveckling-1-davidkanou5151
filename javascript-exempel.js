04ad11509bb0501afb75015e5f4b725ea5337174
david <deved.kanou@elev.ga.ntig.se>
2021-03-23 15:01:53 +0100

    javascript js1.3

--- javascript-exempel.js
+++ javascript-exempel.js
@@ -4,4 +4,15 @@ som kan sträcka sig över flera rader.*/
 //En kommentar på en rad.
 function visaDialog(){
 alert("Hej min vän");
+changeText();
+}
+
+
+function changeText(){
+    document.getElementById("demo").innerHTML = "Hello ladies and gentelman; ";
+}
+
+function writeTewxt(){
+    var namn = document.getElementById("fnamn").value;
+    document.getElementById("text").innerHTML = namn;
 }
